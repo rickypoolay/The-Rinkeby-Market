f48c1e91eb6dece019b7575c8cd6e55164394a24
Ricardo Poullet <rickypoullet@gmail.com>
2022-01-30 05:30:49 -0500

    small fix small fix

--- components/functions.jsx
+++ components/functions.jsx
@@ -14,7 +14,7 @@ export const trunc = (firstStart, firstEnd, secondStart, secondEnd, string) => {
 };
 
 export const fixURL = (url) => {
-  if (url.startsWith("ipfs")) {
+  if (url?.startsWith("ipfs")) {
     return (
       "https://ipfs.moralis.io:2053/ipfs/" + url.split("ipfs://ipfs/").slice(-1)
     );
